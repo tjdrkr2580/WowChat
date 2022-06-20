3c7dea2bb8feb4bdfc71fbc1c2540d3287b36017
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-06-20 20:20:15 +0900

    Wowchat

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,29 +1,48 @@
-import React, { useState } from 'react'
-import io from 'socket.io-client';
-import './App.css';
-import Chat from './Components/Chat';
+import "./App.css";
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Components/Chat";
 
 const socket = io.connect("http://localhost:3001");
 
-const App = () => {
-  const [userName,setUserName] = useState("");
-  const [room,setRoom] = useState("");
+function App() {
+  const [username, setUsername] = useState("");
+  const [room, setRoom] = useState("");
+  const [showChat, setShowChat] = useState(false);
 
   const joinRoom = () => {
-    if(userName !== "" && room !== ""){
+    if (username !== "" && room !== "") {
       socket.emit("join_room", room);
+      setShowChat(true);
     }
-  }
-  return (
-    <div className='App'>
-      <h1>Join Chat</h1>
-      <input type="text" placeholder='John...' onChange={(event) => {setUserName(event.target.value);}}/>
-      <input type="text" placeholder='Room ID...' onChange={(event) => {setRoom(event.target.value);}}/>
-      <button onClick={joinRoom}>Join A Room</button>
+  };
 
-      <Chat socket={socket} userName={userName} room={room}/>
+  return (
+    <div className="App">
+      {!showChat ? (
+        <div className="joinChatContainer">
+          <h3>Wow Chat</h3>
+          <input
+            type="text"
+            placeholder="John..."
+            onChange={(event) => {
+              setUsername(event.target.value);
+            }}
+          />
+          <input
+            type="text"
+            placeholder="Room ID..."
+            onChange={(event) => {
+              setRoom(event.target.value);
+            }}
+          />
+          <button onClick={joinRoom}>Join A Room</button>
+        </div>
+      ) : (
+        <Chat socket={socket} username={username} room={room} />
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -16,10 +16,15 @@ const io = new Server(server, {
 
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
+
     socket.on("join_room",(data)=>{
       socket.join(data);
       console.log(`User With ID: ${socket.id} joined room: ${data}`);
-    })
+    });
+
+    socket.on("send_message", (data) => {
+      socket.to(data.room).emit("receive_message",data);
+    })  
     
     socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id);
